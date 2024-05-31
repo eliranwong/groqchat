577c9f45464155db9ae4b76c88b3d5e552d98bda
eliranwong <hkwebtech@gmail.com>
2024-05-31 11:02:44 +0100

    v0.0.1; first upload; distributed via pypi

---
+++ package/groqchat/utils/promptValidator.py
@@ -0,0 +1,49 @@
+from groqchat import config
+from prompt_toolkit.validation import Validator, ValidationError
+
+class NumberValidator(Validator):
+    def validate(self, document):
+        text = document.text
+
+        if text.lower() in (config.exit_entry, config.cancel_entry):
+            pass
+        elif text and not text.isdigit():
+            i = 0
+
+            # Get index of first non numeric character.
+            # We want to move the cursor here.
+            for i, c in enumerate(text):
+                if not c.isdigit():
+                    break
+
+            raise ValidationError(message='This entry accepts numbers only!', cursor_position=i)
+
+
+class FloatValidator(Validator):
+    def validate(self, document):
+        text = document.text
+
+        if text.lower() in (config.exit_entry, config.cancel_entry):
+            pass
+        try:
+            float(text)
+        except:
+            raise ValidationError(message='This entry accepts floating point numbers only!', cursor_position=0)
+
+
+class NoAlphaValidator(Validator):
+    def validate(self, document):
+        text = document.text
+
+        if text.lower() in (config.exit_entry, config.cancel_entry):
+            pass
+        elif text and text.isalpha():
+            i = 0
+
+            # Get index of first non numeric character.
+            # We want to move the cursor here.
+            for i, c in enumerate(text):
+                if c.isalpha():
+                    break
+
+            raise ValidationError(message='This entry does not accept alphabet letters!', cursor_position=i)
